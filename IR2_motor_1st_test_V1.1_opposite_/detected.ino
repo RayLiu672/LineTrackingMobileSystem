f6ddb6d0504f732e8f15f796e09f34e67ecfe2d6
Ray <rayliu672@gmail.com>
2022-04-18 14:06:48 -0700

    update to Ir_Motor

---
+++ IR2_motor_1st_test_V1.1_opposite_/detected.ino
@@ -0,0 +1,71 @@
+void detected(){
+  // IR sensor left and right triggered
+  if(Ir_Right_Val == 0 && Ir_Left_Val == 0){
+    
+    Serial.println("both detected");
+    // Turning both motors off
+    digitalWrite(LEFT_POS_CONTROL, LOW);
+    digitalWrite(LEFT_NEG_CONTROL, LOW);
+    digitalWrite(RIGHT_POS_CONTROL, LOW);
+    digitalWrite(RIGHT_NEG_CONTROL, LOW);
+  }
+  
+  // IR sensor right triggered
+  else if(Ir_Right_Val == 0 && Ir_Left_Val == 1){    
+    Serial.println("right detected");
+     // Right stop
+    analogWrite(ENB, 0);
+    
+    // Forward Motion
+    digitalWrite(LEFT_POS_CONTROL, HIGH);
+    digitalWrite(LEFT_NEG_CONTROL, LOW);
+    digitalWrite(RIGHT_POS_CONTROL, LOW);
+    digitalWrite(RIGHT_NEG_CONTROL, HIGH); 
+    
+    // increase Left motor Rpm  ------------------------------------------------------------------------------- Take a look a while loop condition ----------------------------------------
+    while(Ir_Right_Val == 0){
+      Ir_Left_Val = digitalRead(IR_LEFT);
+      Ir_Right_Val = digitalRead(IR_RIGHT); 
+      if(Ir_Left_Val == 0){
+        break;
+        }
+      if(Left_Rpm < 255){
+        Left_Rpm++; 
+        //Increase the Left motor RPM
+        analogWrite(ENA, Left_Rpm);
+      }    
+      Serial.println(Left_Rpm);
+      delay(20);
+      }      
+    }
+    
+  // IR sensor left triggered  
+  else if(Ir_Right_Val == 1 && Ir_Left_Val == 0){ 
+    Serial.println("left detected");
+    // left stop
+    analogWrite(ENA, 0);
+    // Forward Motion
+    digitalWrite(LEFT_POS_CONTROL, HIGH);
+    digitalWrite(LEFT_NEG_CONTROL, LOW);
+    digitalWrite(RIGHT_POS_CONTROL, LOW);
+    digitalWrite(RIGHT_NEG_CONTROL, HIGH);
+    
+    // increase Right motor Rpm ------------------------------------------------------------------------------- Take a look a while loop condition ----------------------------------------
+    while(Ir_Left_Val == 0){
+      // Check if robot returned to line
+      Ir_Left_Val = digitalRead(IR_LEFT);
+      Ir_Right_Val = digitalRead(IR_RIGHT);
+      if(Ir_Right_Val == 0){
+        break;
+        }
+      // Increase Right_Rpm
+      if(Right_Rpm < 255){
+        Right_Rpm++;
+        analogWrite(ENB, Right_Rpm);
+      }
+      //Increase the Right motor RPM 
+      Serial.println(Right_Rpm);    
+      delay(20);
+      }             
+    }
+   }
